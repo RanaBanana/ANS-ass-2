d1df50238e5a3b131ae9e926f9d8152dac366c2d
RanaBanana <RanaBanana@users.noreply.github.com>
2020-10-07 12:38:54 +0200

    Add files via upload

--- M_PH_spectra.m
+++ M_PH_spectra.m
@@ -5,14 +5,14 @@ f2 = 50; % Frequency 2 in 50 Hz
 
 %Sampling frequence has to conform to Nyquist theorem, so min of 2*20 =40.
 Fs = 50; 
-T_max = 0.1; 
+T_max = 10; 
 nfft = 1024;
 % Consider the following signals:
 t = 0:1/Fs:T_max;
 
 s1 = sin(2*pi*t*f1); % 5 Hz sine wave
 s2 = sin(2*pi*t*f2); % 50 Hz sine wave
-c = cos(2*pi*t*f2); % 5 Hz sine wave
+c = cos(2*pi*t*f2); % 5 Hz cosine wave
 
 % % And their combinations:
 % x1 = s1*0.8+s2*0.2;
@@ -47,12 +47,34 @@ legend('s1','s2','c')
 %% Results
 
 %Spectra of the waves: s1 has a sine shape, whilst s2 and c are straight
-% lines. 
-%FFT magnitude: s1 has a magnitude at 0, s2 is a horizontal line at 0, c
-% has a magnitude at 0.
-%Phase: 
+% lines at 0 and 1 respectively. 
+%FFT magnitude: s1 has a magnitude at peak 0, s2 is a horizontal line at 0, c
+% has a magnitude peak at 0.
+%Phase: s1 and c have similar phase between -5 and 5 Hz, s2 is slightly
+% different than both.
 % Fs = 50; 
 % T_max = 0.1; 
+% nfft = 1024;
+
+%Spectra of the waves: s1 has a sine shape, whilst s2 and c are straight
+% lines at 0 and 1 respectively. 
+%FFT magnitude: s1 has a magnitutde peak at -5 and 5 Hz, s2 is a straight line,
+% c has a magnitude peak at 0. There is a reasonably lot of leakage.
+%Phase: s1 changes in amplitude between -5 and 5 Hz, s2 doesn't change, c
+% goes 'lower' after 0 Hz.
+% Fs = 50; 
+% T_max = 2; 
+% nfft = 1024;
+
+%Spectra of the waves: s1 has a sine shape, with a higher frequency than
+% previous settings. s2 and c are straight lines at 0 and 1 respectively. 
+%FFT magnitude: s1 has two magnitude peaks at -5 and 5 Hz, s2 is a straight
+% line (doesn't have a peak), c has a peak at 0 Hz. There is a lot less
+% leakage than with the previous settings.
+%Phase: s1 changes in amplitude between -5 and 5 Hz, s2 has a general 
+% oscillating figure, c goes 'lower' after 0 Hz.
+% Fs = 50; 
+% T_max = 10; 
 % nfft = 1024;
 
 

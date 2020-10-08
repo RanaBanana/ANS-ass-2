6714e723507590429ef11b50abf77a21952a09d7
ElineHa <70947715+ElineHa@users.noreply.github.com>
2020-10-08 20:18:26 +0200

    Update PTSH.m

--- PTSH.m
+++ PTSH.m
@@ -49,13 +49,69 @@ end
 % Variable definitions
 window_spike = 250000;
 % lfp_data is expressed in uV.
-LFP = [lfp_data ; lfp_ts];
+LFP_table = table(lfp_data, lfp_ts);
 %Test on first 12000 columns of LFP:
-small_data = LFP(:,1:12000);
-plot(small_data(2,:),small_data(1,:));
+%small_data = LFP(:,1:12000);
+%plot(small_data(2,:),small_data(1,:));
 
 % Plotting the whole LFP signal; WARNING: ONLY RUN ON FAST COMPUTER
 % plot_LFP = plot(LFP(2,:), LFP(1,:))
+
+
+
+% New:
+
+% Compute the 250 ms time window around the spike times (in columns).
+window250_spikes = [spikes_ts-window_spike; spikes_ts+window_spike]';
+
+% Preallocate the window250_lfp and the timewindow_lfp (both have two
+% columns)
+window250_lfp = zeros(length(window250_spikes),2);
+timewindow_lfp = zeros(length(window250_spikes),2);
+
+% Create a transition time window which can shift the window250_spikes to
+% the corresponding time window in the lfp data.
+for i= 1:length(window250_spikes)
+    % Using abs() and min(), it finds the difference of the number that's 
+    % closest to window250_spikes(1,i). 
+    % Iterate through every column of the -250ms timestamps
+    % And create a new variable (window250_lfp) containing these
+    % timeshifts (in columns)
+    window250_lfp(i,1) = min(abs(lfp_ts-window250_spikes(i,1)));
+    window250_lfp(i,2) = min(abs(lfp_ts-window250_spikes(i,2)));
+    
+    % Create an lfp timewindow of -250ms to +250ms around spikes
+    timewindow_lfp(i,1) = window250_spikes(i,1)+window250_lfp(i,1);
+    timewindow_lfp(i,2) = window250_spikes(i,2)+window250_lfp(i,2);
+end
+
+% First for loop: iterates through the different timewindows (of -250 and
+% 250 ms)
+for j=1:length(timewindow_lfp) 
+    % Find the positions corresponding to the values of the -250 and
+    % 250 ms time window.
+    tmin_pos = find(LFP_table.lfp_ts==timewindow_lfp(j,1));
+    tmax_pos = find(LFP_table.lfp_ts==timewindow_lfp(j,2));
+    %Preallocate Volt. This way, it is emptied during every iteration of j.
+    Volt = [];
+    % Second for loop: iterates through the values of lfp_ts and checks if
+    % they fall within the j-th timewindow. 
+    % Iteration from tmin_pos to tmax_pos.
+    for i= tmin_pos:tmax_pos 
+        if LFP_table.lfp_ts(i)>=timewindow_lfp(j,1) && LFP_table.lfp_ts(i)<=timewindow_lfp(j,2)
+            % Create variable containing all Volt values within the time
+            % window. 
+            Volt(i,1)= LFP_table.lfp_data(lfp_ts==LFP_table.lfp_ts(i));
+        end
+    end
+    % Create variable containing the means of each row 
+    Volt_means(j) = mean(Volt(:,1));
+end
+
+
+
+
+
 
 % Stimulus presentation window
 % is timeframe onset-offset of stimulus.

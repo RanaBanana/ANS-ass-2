b844feac3dcfa7fd087491ebc39d64b1baba587e
ElineHa <70947715+ElineHa@users.noreply.github.com>
2020-10-18 23:13:41 +0200

    Updated ex 1.  just Y-axis is not correct yet

--- Assignment2_NN.m
+++ Assignment2_NN.m
@@ -1,84 +1,58 @@
 %Assignment 2
 %Nick, Eline, & Rana 
 %Nervous Neurons
+
 %% Exercise 1
-% Load assignment2_data.mat
+clear all
+
+% load data
+load('assignment2_data.mat')
 
 % Combine the events_ts and events_type to mark when the stimulus was
 % presented.
 events_table = table(events_ts, events_type);
 
-% Set of variables. Set to microseconds to match data.
-timeBin = 25000; % time bin in microseconds
-preStim = 500000; % time before stimulus onset in microseconds
-postStim = 1000000; % time after stimulus offset in microseconds
-
-% Extract values from table corresponding to the on- and offset
+% Extract values from table corresponding to the on- and offset 
 onTimes = events_table.events_ts(events_type==1);
 offTimes = events_table.events_ts(events_type==31);
 
-% Iterate over length of all onset stimulus points (offset could also be
-% done but less intuitive)
-for i = 1:length(onTimes) 
-    % We use find() to acquire all relevant timepoints on the dataset that are inbetween
-    % the onset timing - 500 and offset timing + 1000. We then assign it to
-    % a variable called hit.
-    hit = find(spikes_ts >= (onTimes(i) - preStim) & spikes_ts <= (onTimes(i) + postStim));
-    % we assign a temporary value for the spikestiming - onsettiming 
-    % (REMOVE THIS COMMENT AT THE END - we do this so we can get a general
-    % timepoint each spike is measured *relative!* to the onset of the
-    % stimulus, it gives a handy way to plot it later I hope)
-    spikes_on_time = spikes_ts(hit) - onTimes(i);
-    % has all timepoints per measures spike minus corresponding onset of
-    % stimulus. The value that spikes_on_time get is also used to show the
-    % length needed so we dont have to create alternative loops
-    % (REMOVE THIS COMMENT AT THE END - HOLY SHIT THIS IS UGLY. It works
-    % but we assign a big fucking chunky disaster of a method, as PSTH_time
-    % changes every fucking time we have a different spikes/i so it's omega
-    % inefficiënt. But I'm too braindead to fix this now, remind me when we
-    % have time later or maybe one of you sees a good solution, for now
-    % bear with this integrity violating construct pl0x 8-)).
-    PSTH_time(i, 1:length(spikes_on_time)) = spikes_on_time;
+% Set of variables. Set to microseconds to match data.
+ms_500 = 500000; % 500 ms in microseconds. Used as preStim and stimulus presentation length. 
+postStim = 1000000; % time after stimulus offset in microseconds
+
+% Select PSTH binsize and edges
+
+timeBin = 25000; % time bin in microseconds
+edges = (-500000: timeBin: 1500000);
+
+% PSTH
+spike_count=0;
+for i = 1:length(onTimes) % Loop over all trials
+    % Select spikes during the i-th trial
+    hit = find(spikes_ts >= (onTimes(i) - ms_500) & spikes_ts < (offTimes(i) + postStim));
+    
+    % Calculate spike times relative to stimulus onset
+    relative_spikes = spikes_ts(hit) - onTimes(i);
+    
+    % Calculate number of spikes per bin (25 ms). histcounts() divides the 
+    % relative_spikes into bins specified by edges.
+    spike_count = spike_count+histcounts(relative_spikes,edges);  
 end
 
-% Easy way to change our microseconds to milliseconds.
-PSTH_time = PSTH_time / 1000;
+% Compute average number of spikes per bin ->?
 
-% We do this so we can get rid of the zeros, otherwise it fucks our whole
-% graph because it will count the zeros as hits (this is a result of the
-% moral corruptness mentioned above, it assigns 0's when it's filling in
-% unneccesary space :'). so we fix it with a bandaid as shown below.
-remove_zero = PSTH_time ~= 0;
-% IMPORTANT NOTE: yo Nick your bandaid fucking sucks. Ik zag net op
-% whatsapp dat de frequency count 300 verschilt met en zonder filter. Not
-% sure how maar maybe ramt hij ontzettend kleine getallen bij 0 er ook uit.
-% I'll take a look at it tomorrow
+% Divide by total length of PSTH to compute firing rate (= estimated spikes 
+% per second. In Hz, so timeBin in seconds)
+firing_rate = spike_count/(length(onTimes)*(timeBin/10^3));
 
-% Dumb name but we use to create a vector of 50 to 100 with so many fucking
-% steps it gets high enough in our histogram to make the orange bar. I
-% wanted it in and got stubborn to show where the stimulus duration was
-% present. It's ugly now but nothing crying cant fix
-testing = (50:0.08:100);
-
-% All the outward stuff to make the histogram, first half stolen off
-% Eline's code. Rest is self explanatory except for hold on/off maybe. We
-% use it to be able to push two plots on top of eachother, they don't like
-% eachother I'm pretty sure but using the hold on cheat code gave them no
-% choice. If they start fighting blame Matlab please.
-figure
-histogram(PSTH_time(remove_zero), 80)
-xlabel('Time (milliseconds)', 'FontSize', 12);
-ylabel('Spike Count', 'FontSize', 12);
-axis tight
-title('Spike frequency per 25 ms time bin', 'FontSize', 14);
-hold on
-histogram(testing, 1)
-hold off
-% To be honest not sure why the legend function works as this. It's
-% strangely fucking neat it assigns the appropriate colors to the calls.
-% Kinda cool
-legend('Spikes per timebin', 'timeframe of stimulus');
-
+% Plot PSTH
+figure('Name','Exercise 1firing_rate','NumberTitle','off')
+t_freq = linspace(-500,1500, length(firing_rate));
+bar(t_freq,firing_rate);
+xlabel('Time from stimulus onset (s)', 'FontSize', 12);
+ylabel('Firing Rate (Hz)', 'FontSize', 12);
+title('PSTH of audiovisual stimulus', 'FontSize', 14);
+ 
 %% Exercise 2
 % Evaluate whether spiking activity during stimulus presentation period (the period
 % from stimulus onset to 500 ms afterwards) is significantly different from baseline
